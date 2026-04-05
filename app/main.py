4d6c9f599d8e367b8703c2f4558ae6311a8bfbbf
Dima Sukhariev <dmitriu.sy1303@gmail.com>
2026-04-05 23:20:53 +0300

    fix flake8

--- app/main.py
+++ app/main.py
@@ -1,10 +1,9 @@
 class Person:
     people = {}
-    def __init__(self, name: str, age: int):
+    def __init__(self, name: str, age: int) -> None:
         self.name = name
         self.age = age
         Person.people[name] = self
-
 
 def create_person_list(people: list) -> list:
     result = []
@@ -14,10 +13,10 @@ def create_person_list(people: list) -> list:
         person_obj = Person(name=item["name"], age=item["age"])
         result.append(person_obj)
 
-    for t in people:
-        person = Person.people[t["name"]]
+    for person_dict in people:
+        person = Person.people[person_dict["name"]]
         for key in ("wife", "husband"):
-            partner_name = t.get(key)
+            partner_name = person_dict.get(key)
             if partner_name is not None:
                 setattr(person, key, Person.people[partner_name])
     return result
